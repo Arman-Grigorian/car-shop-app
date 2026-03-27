457cc3563c1070d71d7eba186118c0624c3c449c
Arman-Grigorian <armyan897@gmail.com>
2026-03-27 20:25:00 +0400

    Initial commit - Car Shop app

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,11 @@ from flask import Blueprint, render_template
 
 main = Blueprint("main", __name__)
 
+# Sample cars
 cars = [
-    {"name": "Tesla Model 3", "price": "$40,000"},
-    {"name": "BMW M3", "price": "$72,000"},
-    {"name": "Toyota Supra", "price": "$55,000"},
+    {"name": "Tesla Model 3", "price": "$40,000", "image": "https://tesla-cdn.thron.com/delivery/public/image/tesla/Model3.jpg"},
+    {"name": "BMW M3", "price": "$72,000", "image": "https://cdn.bmwblog.com/wp-content/uploads/2018/03/BMW-M3-F80-Competition-Review-7.jpg"},
+    {"name": "Toyota Supra", "price": "$55,000", "image": "https://cdn.motor1.com/images/mgl/0ANPP/s1/2020-toyota-supra.jpg"}
 ]
 
 @main.route("/")
